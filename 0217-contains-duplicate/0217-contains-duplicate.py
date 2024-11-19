a39ba6595de53140ad682a633c6fc8e48f9e1faa
Iuri Henrique <99624350+iurihenriq@users.noreply.github.com>
2024-11-19 20:57:01 -0300

    Time: 15 ms (37.34%), Space: 25.3 MB (49.69%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def containsDuplicate(self, nums):
+        nums_set = set()
+
+        for num in nums:
+            if num in nums_set:
+                return True
+            nums_set.add(num)
+            
+        return False
+
+        

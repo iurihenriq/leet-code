3e8080969d222d8fba2b740765a57e5db4f4c579
Iuri Henrique <99624350+iurihenriq@users.noreply.github.com>
2024-11-21 12:04:02 -0300

    Time: 25 ms (42.43%), Space: 17.3 MB (27.88%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def groupAnagrams(self, strs):
+        map_string = {}  # Dicionário para armazenar grupos de anagramas
+
+        for string in strs:
+            count = [0] * 26
+            
+            for letter in string:
+                count[ord(letter) - ord('a')] += 1
+            
+            key = tuple(count)
+
+            if key not in map_string:
+                map_string[key] = [string]
+            else:
+                map_string[key].append(string)
+
+        return list(map_string.values())

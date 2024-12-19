386dce014792f6bcd68998b653a379f46a7f862b
Iuri Henrique <99624350+iurihenriq@users.noreply.github.com>
2024-12-18 21:54:17 -0300

    Time: 3 ms (90.36%), Space: 13.7 MB (5.25%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def moveZeroes(self, nums):
+        non_zero_index = 0
+
+        for i in range(len(nums)):
+            if nums[i] != 0:
+                nums[non_zero_index] = nums[i]
+                non_zero_index += 1
+
+        for i in range(non_zero_index, len(nums)):
+            nums[i] = 0
